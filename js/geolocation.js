d735c7c2c28bf83a18746f93d773b88e514571d9
Maryam Arab <maryam.arab83@gmail.com>
2018-02-16 11:10:16 -0500

    first commit in second branch

---
+++ js/geolocation.js
@@ -0,0 +1,8 @@
+function init() {
+    var mapOptions = {                                 // Set up the map options
+        center: new google.maps.LatLng(40.782710,-73.965310),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 13
+    };
+   
+}
