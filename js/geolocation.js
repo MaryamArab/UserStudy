4a6fd2e970b47ea542686766c42a45ac87ebb710
Maryam Arab <maryam.arab83@gmail.com>
2018-02-19 09:36:25 -0500

    test 4

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,7 +2,7 @@ function init() {
     var mapOptions = {                                 // Set up the map options
         center: new google.maps.LatLng(40.782710,-73.965310),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 2.2
+        zoom: 22
     };
    
 }
